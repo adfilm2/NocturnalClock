85fc9450883775efec7a89a8472bd48cec94557f
dohan <adfilm2@naver.com>
2021-06-10 23:43:24 +0900

    기초 레이아웃

--- TimeApp/ViewController.swift
+++ TimeApp/ViewController.swift
@@ -6,14 +6,27 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
 
+    
+    
+    
+    @IBOutlet weak var clock: UILabel!
+    @IBOutlet weak var wokeUpButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setLayout()
     }
 
+    func setLayout(){
+  
+        wokeUpButton.layer.cornerRadius = 10
+    }
 
 }
 
